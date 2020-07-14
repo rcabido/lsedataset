8b7f2b9e647400c5942fbc43e51b1c360c2e296d
RubenBernabe <37367277+RubenBernabe@users.noreply.github.com>
2020-07-14 23:45:43 +0200

    Fix pytubelib

--- bugPytube.sh
+++ bugPytube.sh
@@ -1,2 +1,4 @@
-rm /usr/local/lib/python3.6/dist-packages/pytube/mixins.py 
-mv mixins.py /usr/local/lib/python3.6/dist-packages/pytube/mixins.py 
+rm /usr/local/lib/python3.6/dist-packages/pytube/__main__.py 
+mv __main__.py /usr/local/lib/python3.6/dist-packages/pytube/__main__.py 
+rm /usr/local/lib/python3.6/dist-packages/pytube/contrib/playlist.py 
+mv playlist.py /usr/local/lib/python3.6/dist-packages/pytube/contrib/playlist.py 
